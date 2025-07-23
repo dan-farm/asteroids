625471a28353c8fe242930140b5f8e0250d0cee6
dan-farm <160326068+dan-farm@users.noreply.github.com>
2025-07-24 09:25:29 +1000

    updateable and drawable groups in main.py

--- main.py
+++ main.py
@@ -11,14 +11,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updateables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    Player.containers = (updateables, drawables)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+        updateables.update(dt)
         pygame.Surface.fill(screen,(0,0,0))
-        player.draw(screen)
+        for drawable in drawables:
+            drawable.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 if __name__ == "__main__":
